15ad85eb1a32b8c63ec47308c4e276b589b57ae1
pnpdjie <542574439@QQ.COM>
2017-06-24 16:36:52 +0800

    Update CheckBoxTreeNode.java
    选择子节点 or父节点 逻辑

--- MDFilesCheckUtils/src/src/CheckBoxTreeNode.java
+++ MDFilesCheckUtils/src/src/CheckBoxTreeNode.java
@@ -29,61 +29,78 @@ public class CheckBoxTreeNode extends DefaultMutableTreeNode {
 
     public void setSelected(boolean _isSelected) {
         this.isSelected = _isSelected;
-
-        if (_isSelected) {
-            // 如果选中，则将其所有的子结点都选中
-            if (children != null) {
-                for (Object obj : children) {
-                    CheckBoxTreeNode node = (CheckBoxTreeNode) obj;
-                    if (_isSelected != node.isSelected())
-                        node.setSelected(_isSelected);
-                }
-            }
-            // 向上检查，如果父结点的所有子结点都被选中，那么将父结点也选中
-            CheckBoxTreeNode pNode = (CheckBoxTreeNode) parent;
-            // 开始检查pNode的所有子节点是否都被选中
-            if (pNode != null) {
-                int index = 0;
-                for (; index < pNode.children.size(); ++index) {
-                    CheckBoxTreeNode pChildNode = (CheckBoxTreeNode) pNode.children.get(index);
-                    if (!pChildNode.isSelected())
-                        break;
-                }
-                /*
-                 * 表明pNode所有子结点都已经选中，则选中父结点， 该方法是一个递归方法，因此在此不需要进行迭代，因为
-                 * 当选中父结点后，父结点本身会向上检查的。
-                 */
-                if (index == pNode.children.size()) {
-                    if (pNode.isSelected() != _isSelected)
-                        pNode.setSelected(_isSelected);
-                }
-            }
-        } else {
-            /*
-             * 如果是取消父结点导致子结点取消，那么此时所有的子结点都应该是选择上的；
-             * 否则就是子结点取消导致父结点取消，然后父结点取消导致需要取消子结点，但 是这时候是不需要取消子结点的。
-             */
-            if (children != null) {
-                int index = 0;
-                for (; index < children.size(); ++index) {
-                    CheckBoxTreeNode childNode = (CheckBoxTreeNode) children.get(index);
-                    if (!childNode.isSelected())
-                        break;
-                }
-                // 从上向下取消的时候
-                if (index == children.size()) {
-                    for (int i = 0; i < children.size(); ++i) {
-                        CheckBoxTreeNode node = (CheckBoxTreeNode) children.get(i);
-                        if (node.isSelected() != _isSelected)
-                            node.setSelected(_isSelected);
-                    }
-                }
-            }
-
-            // 向上取消，只要存在一个子节点不是选上的，那么父节点就不应该被选上。
-            CheckBoxTreeNode pNode = (CheckBoxTreeNode) parent;
-            if (pNode != null && pNode.isSelected() != _isSelected)
-                pNode.setSelected(_isSelected);
-        }
+        // 根据选中条件添加判断逻辑,如果都没选，就只勾选当前节点
+		if (type.equals(R.SUBNODES)) {
+			// 只选中Auto Select Subnodes，自动勾选所有子集节点
+			subnodesSelect(_isSelected, type);
+		} else if (type.equals(R.PARENT)) {
+			// 只选中Auto Select Parent，自动勾选父级节点即使子集节点未全部勾选
+			parentSelect(_isSelected, type);
+		} else if (type.equals(R.SUBNODES_AND_PARENT)) {
+			// 选中Auto Select Subnodes 和 Auto Select
+			// Parent，自动勾选所有子集节点并勾选父级节点即使子集节点未全部勾选
+			subnodesAndParentSelect(_isSelected, type);
+		}
     }
+    /*
+	 * 选中Auto Select Subnodes 和 Auto Select Parent，自动勾选所有子集节点并勾选父级节点即使子集节点未全部勾选
+	 */
+	private void subnodesAndParentSelect(boolean _isSelected, String type) {
+		  subnodesSelect(_isSelected, R.SUBNODES);
+		  parentSelect(_isSelected, R.PARENT);
+	}
+
+	/*
+	 * 只选中Auto Select Parent，自动勾选父级节点即使子集节点未全部勾选
+	 */
+	private void parentSelect(boolean _isSelected, String type) {
+		if (_isSelected) {
+			// 向上检查，如果父结点的子结点被选中，那么将父结点也选中
+			CheckBoxTreeNode pNode = (CheckBoxTreeNode) parent;
+			// 开始检查pNode的子节点是否被选中
+			if (pNode != null) {
+				int index = 0;
+				for (; index < pNode.children.size(); ++index) {
+					CheckBoxTreeNode pChildNode = (CheckBoxTreeNode) pNode.children.get(index);
+					if (pChildNode.isSelected()) {
+						pNode.setSelected(_isSelected, type);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	/*
+	 * 只选中Auto Select Subnodes，自动勾选所有子集节点
+	 */
+	private void subnodesSelect(boolean _isSelected, String type) {
+		if (_isSelected) {
+			// 如果选中，则将其所有的子结点都选中
+			if (children != null) {
+				for (Object obj : children) {
+					CheckBoxTreeNode node = (CheckBoxTreeNode) obj;
+					if (_isSelected != node.isSelected())
+						node.setSelected(_isSelected, type);
+				}
+			}
+		} else {
+			if (children != null) {
+				int index = 0;
+				for (; index < children.size(); ++index) {
+					CheckBoxTreeNode childNode = (CheckBoxTreeNode) children.get(index);
+					if (!childNode.isSelected())
+						break;
+				}
+				// 从上向下取消的时候
+				if (index == children.size()) {
+					for (int i = 0; i < children.size(); ++i) {
+						CheckBoxTreeNode node = (CheckBoxTreeNode) children.get(i);
+						if (node.isSelected() != _isSelected)
+							node.setSelected(_isSelected, type);
+					}
+				}
+			}
+		}
+	}
 }
